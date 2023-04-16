8dc2e71b5d2c23a865de198997580874c0cb9d70
leskovskiy <116192490+leskovskiy@users.noreply.github.com>
2023-04-16 13:55:52 +0700

    Add files via upload

---
+++ oop/src/main/java/homwork2/Wall.java
@@ -0,0 +1,17 @@
+package homwork2;
+
+public class Wall {
+
+   private Integer hight ;
+
+    public Wall(Integer hight) {
+        this.hight = hight;
+    }
+
+    public Wall() {
+    }
+
+    public Integer getHight() {
+        return hight;
+    }
+}
